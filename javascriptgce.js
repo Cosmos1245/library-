032cc130cffc83ecb71a26b309109886de9409cb
Cosmos1245 <167671233+Cosmos1245@users.noreply.github.com>
2024-10-09 20:36:33 +0530

    javascriptgce.js

--- javascriptgce.js
+++ javascriptgce.js
@@ -1,13 +1,21 @@
 let container=document.getElementById("overall");
 document.getElementById("home").addEventListener("click",function(){
-    let h=document.createElement("h1");
-    h.textContent="success";
-    container.appendChild(h);
+   
 })
 let themeSelector=document.getElementById("themeSelector");
-themeSelector.addEventListener("click",function(){
+themeSelector.addEventListener("click",function(event){  
+    let textofanchor=document.querySelectorAll(".nav-link, .navbar-brand");
     let theme=document.getElementById("theme");
-    let a=document.getElementById("main");
     theme.classList.toggle("bg-dark");
-    a.classList.toggle("dark-theme");
+    if(event.target.value==="Dark Theme"){
+        textofanchor.forEach(anchor => {
+            anchor.style.color="white" ;
+        });
+    }
+    else{
+        textofanchor.forEach(anchor => {
+            anchor.style.color="black" ;
+        });
+    }
+    
 })
